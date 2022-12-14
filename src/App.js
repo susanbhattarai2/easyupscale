c02a71d37f319ef9101c4af2059ae4c36d871ca4
SusanBhattarai <susanbhattrai36@gmail.com>
2022-12-15 02:56:59 +0545

    [Added] businessNavbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Home from './pages/home/Home'
 import Client from './pages/clients/Clients'
 import Business from './pages/business/Business'
+import About from './pages/about/About'
+import Contact from './pages/contact/Contact'
+import Blog from './pages/blog/Blog'
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
@@ -12,7 +15,9 @@ function App() {
         <Route path='/' element ={<Home />} />
         <Route path='/business' element={<Business />}/>
         <Route path='/client' element={<Client />}/>
-
+        <Route path='/about' element={<About />}  />
+        <Route path='/contact'element={<Contact />} />
+        <Route path='/blog' element={<Blog />} />
       </Routes>
      
       
---
+++ src/pages/business/Business.jsx
@@ -0,0 +1,57 @@
+import React,{useState} from 'react'
+import { NavLink } from 'react-router-dom'
+import logo from '../../assets/logo.png'
+import {FaAlignJustify} from 'react-icons/fa'
+import {AiOutlineClose} from 'react-icons/ai'
+const Business = () => {
+    const [nav,setNav] = useState(false);
+    const handleNav = () => {
+        setNav(!nav)
+    };
+
+    return (
+        <>
+
+            <div className='flex justify-between items-center h-20 px-4'>
+                <div >
+                    <img src={logo} alt='logo' height={50} width={150}  />
+                </div>
+                <ul className='md:flex hidden '>
+                    <li className='p-4' >
+                        <NavLink to="/about">About</NavLink>
+                    </li>
+                    <li className='p-4'>
+                        <NavLink to="/blog">Blog</NavLink>
+                    </li>
+                    <li className='p-4'>
+                        <NavLink to ="/contact">Contact</NavLink>
+                    </li>
+                </ul>
+                {/* Hamberger */}
+                <div onClick={handleNav} className='md:hidden z-10'>
+                    { nav ? <AiOutlineClose size={20}/> : <FaAlignJustify size={20}/>}
+                 
+                </div> 
+                {/* mobile menu */}
+                <div onClick={handleNav } className={nav ? 'absolute left-0 top-0 w-full bg-nav px-4 py-7 flex flex-col': 'absolute left-[-100%]'}>
+                <ul>
+                    <img src={logo} alt='logo' height={50} width={150} />
+                    <li className='p-4' >
+                        <NavLink to="/about">About</NavLink>
+                    </li>
+                    <li className='p-4 '>
+                        <NavLink to="/blog">Blog</NavLink>
+                    </li>
+                    <li className='p-4 '>
+                        <NavLink to ="/contact">Contact</NavLink>
+                    </li>
+                </ul>
+                </div>
+            </div>
+
+
+        </>
+    )
+}
+
+export default Business
